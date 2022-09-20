cf1cbeb318c106d8159106f096a53dda7e397e84
Theofilos Petsios <theofilos@crashoverride.com>
2022-09-20 11:46:01 +0300

    options nits

--- config.lua
+++ config.lua
@@ -1,56 +1,57 @@
 vim.opt.backup = false -- creates a backup file
 vim.opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
-vim.opt.colorcolumn = "99999" -- fixes indentline for now
+vim.opt.colorcolumn = "80"
 vim.opt.completeopt = { "menuone", "noselect" }
 vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+vim.opt.cursorline = true -- highlight the current line
+vim.opt.diffopt = "filler,iwhite" -- In diff mode, ignore whitespace,
+vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.fileencoding = "utf-8" -- the encoding written to a file
-vim.opt.foldmethod = "manual" -- folding set to "expr" for treesitter based folding
 vim.opt.foldexpr = "" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
+vim.opt.foldmethod = "manual" -- folding set to "expr" for treesitter based folding
+vim.opt.formatoptions = "cqtr"
 vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
 vim.opt.hidden = true -- required to keep multiple buffers and open multiple buffers
+vim.opt.history = 1000
 vim.opt.hlsearch = true -- highlight all matches on previous search pattern
 vim.opt.ignorecase = true -- ignore case in `search patterns
 vim.opt.mouse = "a" -- allow the mouse to be used in neovim
+vim.opt.number = true -- set numbered lines
+vim.opt.numberwidth = 4 -- set number column width to 2 {default 4}
 vim.opt.pumheight = 10 -- pop up menu height
-vim.opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
+vim.opt.relativenumber = false -- set relative numbered lines
+vim.opt.scrolloff = 3 -- Start scrolling 8 lines before win. border
+vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
 vim.opt.showmatch = true -- we don't need to see things like -- INSERT -- anymore
+vim.opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
 vim.opt.showtabline = 2 -- always show tabs
-vim.opt.splitright = true -- open new pages on the right split when vsplitting
+vim.opt.signcolumn = "yes" -- always show the sign column otherwise it would shift the text each time
 vim.opt.smartcase = true -- smart case
 vim.opt.smartindent = true -- make indenting smarter again
+vim.opt.spell = false
+vim.opt.spelllang = "en"
 vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitbelow = true -- open new pages on the bottom when splitting
 vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.splitright = true -- open new pages on the right split when vsplitting
 vim.opt.swapfile = false -- creates a swapfile
+vim.opt.tabstop = 2 -- insert 2 spaces for a tab
 vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
 vim.opt.timeoutlen = 100 -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.title = true -- set the title of window to the value of the titlestring
 vim.opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
-vim.opt.undodir = vim.fn.stdpath "cache" .. "/undo"
+vim.opt.undodir = os.getenv("HOME") .. "/.config/lvim/undo"
 vim.opt.undofile = true -- enable persistent undo
 vim.opt.undolevels = 10000
 vim.opt.updatetime = 300 -- faster completion
-vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
-vim.opt.expandtab = true -- convert tabs to spaces
-vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 2 -- insert 2 spaces for a tab
-vim.opt.cursorline = true -- highlight the current line
-vim.opt.number = true -- set numbered lines
-vim.opt.relativenumber = false -- set relative numbered lines
-vim.opt.numberwidth = 4 -- set number column width to 2 {default 4}
-vim.opt.signcolumn = "yes" -- always show the sign column otherwise it would shift the text each time
 vim.opt.wrap = false -- display lines as one long line
-vim.opt.spell = false
-vim.opt.spelllang = "en"
-vim.opt.scrolloff = 3 -- Start scrolling 8 lines before win. border
-vim.opt.colorcolumn = "80"
-vim.opt.history = 1000
-vim.opt.diffopt = "filler,iwhite" -- In diff mode, ignore whitespace,
+vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
 
 
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "monokain" -- try onedarker for a non-black background
+lvim.colorscheme = "onedarker" -- try onedarker for a non-black background
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -166,6 +167,7 @@ require("lvim.lsp.manager").setup("codeqlls", {
     search_path = {
       "~/projects/codeql-home",
       "~/projects/codeql-home/codeql-repo",
+      "~/.codeql/packages",
     };
   };
 })
@@ -246,6 +248,9 @@ lvim.plugins = {
   },
   {
     "mbbill/undotree",
+    config = function()
+      vim.cmd("let g:gundo_close_on_revert=1")
+    end,
   },
   {
     "flazz/vim-colorschemes",
