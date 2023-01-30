af60051d198c3ed3c93953bafb21c680d7e8ad01
Theofilos Petsios <theofilos@crashoverride.com>
2023-01-30 20:08:16 +0200

    null-ls

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,4 +1,3 @@
-
 local cmd = vim.cmd
 -- Set options (global/buffer/windows-scoped)
 local opt = vim.opt
@@ -11,12 +10,14 @@ cmd([[
 	filetype plugin indent on
 ]])
 
-opt.backspace = {'eol', 'start', 'indent'} -- allow backspacing over everything in insert mode
-opt.clipboard = 'unnamedplus' -- allow neovim to access the system clipboard
+opt.backspace = { "eol", "start", "indent" } -- allow backspacing over everything in insert mode
+opt.clipboard = "unnamedplus" -- allow neovim to access the system clipboard
 vim.opt.fileencoding = "utf-8" -- the encoding written to a file
-opt.encoding = 'utf-8' -- the encoding
-opt.matchpairs = {'(:)', '{:}', '[:]', '<:>'}
-opt.syntax = 'enable'
+opt.encoding = "utf-8" -- the encoding
+opt.matchpairs = { "(:)", "{:}", "[:]", "<:>" }
+opt.syntax = "enable"
+
+opt.shortmess = aoOtI
 
 -- indention
 opt.autoindent = true -- auto indentation
@@ -31,7 +32,7 @@ opt.shiftround = true -- use multiple of shiftwidth when indenting with '<' and 
 opt.hlsearch = true -- highlight all matches on previous search pattern
 opt.ignorecase = true -- ignore case in search patterns
 opt.smartcase = true -- smart case
-opt.wildignore = opt.wildignore + {'*/node_modules/*', '*/.git/*', '*/vendor/*'}
+opt.wildignore = opt.wildignore + { "*/node_modules/*", "*/.git/*", "*/vendor/*" }
 opt.wildmenu = true -- make tab completion for files/buffers act like bash
 
 -- ui
@@ -42,23 +43,20 @@ opt.list = true
 
 -- You can also add 'space' or 'eol', but I feel it's quite annoying
 opt.listchars = {
-    tab = '┊ ',
-    trail = '·',
-    extends = '»',
-    precedes = '«',
-    nbsp = '×'
+    tab = "┊ ",
+    trail = "·",
+    extends = "»",
+    precedes = "«",
+    nbsp = "×",
 }
 
 opt.colorcolumn = "80"
 
--- Hide cmd line
-opt.cmdheight = 0 -- more space in the neovim command line for displaying messages
-
-opt.mouse = 'a' -- allow the mouse to be used in neovim
+opt.mouse = "a" -- allow the mouse to be used in neovim
 opt.number = true -- set numbered lines
 -- opt.scrolloff = 18 -- minimal number of screen lines to keep above and below the cursor
 -- opt.sidescrolloff = 3 -- minimal number of screen columns to keep to the left and right (horizontal) of the cursor if wrap is `false`
-opt.signcolumn = 'yes' -- always show the sign column, otherwise it would shift the text each time
+opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
 opt.splitbelow = true -- open new split below
 opt.splitright = true -- open new split to the right
 opt.wrap = false -- display a long line
@@ -69,9 +67,9 @@ opt.swapfile = false -- creates a swapfile
 opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 
 -- autocomplete
-opt.completeopt = {'menu', 'menuone', 'noselect'} -- mostly just for cmp
+opt.completeopt = { "menu", "menuone", "noselect" } -- mostly just for cmp
 opt.shortmess = opt.shortmess + {
-    c = true
+    c = true,
 } -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
 
 -- By the way, -- INSERT -- is unnecessary anymore because the mode information is displayed in the statusline.
@@ -90,22 +88,38 @@ opt.termguicolors = true -- enable 24-bit RGB colors
 
 -- persistent undo
 -- Don't forget to create folder $HOME/.local/share/nvim/undo
-local undodir = vim.fn.stdpath('data') .. '/undo'
+local undodir = vim.fn.stdpath("data") .. "/undo"
 opt.undofile = true -- enable persistent undo
 opt.undodir = undodir
 opt.undolevels = 1000
 opt.undoreload = 10000
 
 -- fold
-opt.foldmethod = 'marker'
+opt.foldmethod = "marker"
 opt.foldlevel = 99
 
 -- Disable builtin plugins
-local disabled_built_ins = {"2html_plugin", "getscript", "getscriptPlugin",
-"logipat", "netrw", "netrwPlugin", "netrwSettings", "netrwFileHandlers",
-"matchit", "tar", "rrhelper", "spellfile_plugin", 
-"tutor", "rplugin", "synmenu", "optwin", "compiler",
-"bugreport", "ftplugin"}
+local disabled_built_ins = {
+    "2html_plugin",
+    "getscript",
+    "getscriptPlugin",
+    "logipat",
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "matchit",
+    "tar",
+    "rrhelper",
+    "spellfile_plugin",
+    "tutor",
+    "rplugin",
+    "synmenu",
+    "optwin",
+    "compiler",
+    "bugreport",
+    "ftplugin",
+}
 
 for _, plugin in pairs(disabled_built_ins) do
     g["loaded_" .. plugin] = 1
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,22 +1,22 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-                                  install_path})
-    vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
+    packer_bootstrap =
+        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.o.runtimepath = vim.fn.stdpath("data") .. "/site/pack/*/start/*," .. vim.o.runtimepath
 end
 
 -- Autocommand that reloads neovim whenever you save file
-vim.cmd [[
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost loader.lua source <afile> | PackerSync
   augroup end
-]]
+]])
 
 -- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, 'packer')
+local status_ok, packer = pcall(require, "packer")
 if not status_ok then
     return
 end
@@ -24,104 +24,118 @@ end
 -- Install plugins
 return packer.startup({
     function(use)
-        use 'wbthomason/packer.nvim' -- packer can manage itself
+        use("wbthomason/packer.nvim") -- packer can manage itself
 
-        -- status line
-        use {
-            "hoob3rt/lualine.nvim",
-            after = "github-nvim-theme",
-            config = function()
-                require("lualine").setup {
-                    options = {
-                        theme = "auto" -- or you can assign github_* themes individually.
-                        -- ... your lualine config
-                    }
-                }
-            end
-        }
+        -- fuzzy finder with ,ff etc.
+        use({
+            "nvim-telescope/telescope.nvim",
+            requires = { { "nvim-lua/plenary.nvim" } },
+        })
 
-        use {
-            'nvim-telescope/telescope.nvim', 
-            requires = { {'nvim-lua/plenary.nvim'} }
-        }
-
-        -- parser support
-        use {
-            'nvim-treesitter/nvim-treesitter',
-            requires = {
-                "nvim-treesitter/nvim-treesitter-refactor",
-            },
+        -- parser support and syntax highlight
+        use({
+            "nvim-treesitter/nvim-treesitter",
             run = ":TSUpdate",
             config = function()
                 require("nvim-treesitter.configs").setup({
-                    -- built-in modules
+                    ensure_installed = "all",
                     highlight = {
                         enable = true,
-                        use_languagetree = true,
-                        disable = {},
                     },
-                    indent = {
-                        enable = true,
-                        disable = {},
-                    },
-                    autopairs = {
-                        enable = true,
-                    },
-                    rainbow = {
-                        enable = true,
-                        extended_mode = true,
-                    },
-
-                    -- additinal plugins
-                    refactor = {
-                        highlight_definitions = {
-                            enable = true,
-                        },
-                        highlight_current_scope = {
-                            enable = false,
-                        },
-                    },
-                    ensure_installed = "all",
                     ignore_install = { "phpdoc" },
                 })
             end,
-        }
+        })
 
-        use {
+        use({
+            "numToStr/Comment.nvim",
+            keys = {
+                { "n", "gc" },
+                { "n", "gb" },
+                { "v", "gc" },
+                { "v", "gb" },
+            },
+            config = function()
+                require("Comment").setup({
+                    padding = true,
+                    sticky = true,
+                    ignore = "^$",
+                    toggler = {
+                        line = "gcc",
+                        block = "gbc",
+                    },
+                    opleader = {
+                        line = "gc",
+                        block = "gb",
+                    },
+                    extra = {
+                        above = "gcO",
+                        below = "gco",
+                        eol = "gcA",
+                    },
+                    mappings = {
+                        basic = true,
+                        extra = true,
+                        extended = false,
+                    },
+                    pre_hook = nil,
+                    post_hook = nil,
+                })
+            end,
+        })
+
+        use({
             "projekt0n/github-nvim-theme",
             config = function()
                 require("github-theme").setup({
-                    theme_style = "dark_default",
-                    overrides = function(c)
-                        return {
-                            ColorColumn = { bg = "#222222" }
-                        }
-                    end
+                    theme_style = "dimmed",
+                    -- overrides = function(c)
+                    --     return {
+                    --         ColorColumn = { bg = "#222222" },
+                    --         Normal = { bg = "#0d1117" }
+                    --     }
+                    -- end
                 })
-            end
-        }
+            end,
+        })
 
-        use {
-            'windwp/nvim-autopairs',
+        use({
+            "windwp/nvim-autopairs",
             config = function()
-                require('nvim-autopairs').setup()
-            end
-        }       
+                require("nvim-autopairs").setup()
+            end,
+        })
 
-        use {
-            'nvim-tree/nvim-tree.lua',
+        use({
+            "nvim-tree/nvim-tree.lua",
             requires = {
-                'nvim-tree/nvim-web-devicons',
-                opt = true
+                "nvim-tree/nvim-web-devicons",
+                opt = true,
             },
             config = function()
                 require("config.nvim-tree")
             end,
-        }
+        })
+
+        use({
+            "ludovicchabant/vim-gutentags",
+            config = function()
+                if vim.fn.executable("fd") == 0 then
+                    return function() end
+                end
+
+                if vim.fn.executable("ctags") == 0 then
+                    return function() end
+                end
+
+                vim.g.gutentags_define_advanced_commands = true
+                vim.g.gutentags_file_list_command = "fd --strip-cwd-prefix"
+            end,
+        })
 
         -- colors in terminal with colortoggle
-        use {
-            'norcalli/nvim-colorizer.lua',
+        use({
+            "norcalli/nvim-colorizer.lua",
             config = function()
                 require("colorizer").setup({ "*" }, {
                     RGB = true, -- #RGB hex codes
@@ -132,78 +146,86 @@ return packer.startup({
                     css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
                     css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
                 })
-            end
-        }
+            end,
+        })
 
-        use{
+        use({
             "iamcco/markdown-preview.nvim",
-            run = function() vim.fn["mkdp#util#install"]() end,
-        }
+            run = function()
+                vim.fn["mkdp#util#install"]()
+            end,
+        })
 
         -- faster lua startup
-        use {
-            'lewis6991/impatient.nvim',
+        use({
+            "lewis6991/impatient.nvim",
             config = function()
                 require("impatient")
-            end
-        }
+            end,
+        })
 
         -- Statusline
         -- A blazing fast and easy to configure neovim statusline plugin written in pure lua.
-        use {
-            'nvim-lualine/lualine.nvim',
-            requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+        use({
+            "nvim-lualine/lualine.nvim",
+            after = "github-nvim-theme",
+            requires = { "kyazdani42/nvim-web-devicons", opt = true },
             config = function()
-                require("lualine").setup {
+                require("lualine").setup({
                     options = {
-                        theme = "auto" -- or you can assign github_* themes individually.
+                        theme = "auto", -- or you can assign github_* themes individually.
                         -- ... your lualine config
-                    }
-                }
-            end
-        }
+                    },
+                })
+            end,
+        })
 
-        use {
+        use({
             "simnalamburt/vim-mundo",
             config = function()
                 require("config.mundo")
-            end
-        }
+            end,
+        })
 
-        -- use Mason + null-ls + cmp for language servers / linters / formatters
-        use {
-            'williamboman/mason.nvim',
+        -- lsp
+        use({
+            "VonHeikemen/lsp-zero.nvim",
+            branch = "v1.x",
             requires = {
                 -- LSP Support
-                {'williamboman/mason-lspconfig.nvim'}, -- Optional
-                {'neovim/nvim-lspconfig'},             -- Required
-                {"jose-elias-alvarez/null-ls.nvim"},
-                --{"folke/trouble.nvim"},
-                --{"folke/lsp-colors.nvim"},
-                --{"weilbith/nvim-code-action-menu"},
-                {"ray-x/lsp_signature.nvim"},
-                --{"folke/neodev.nvim"},
-                {"nvim-lua/plenary.nvim"},
+                { "neovim/nvim-lspconfig" }, -- Required
+                { "williamboman/mason.nvim" }, -- Optional
+                { "williamboman/mason-lspconfig.nvim" }, -- Optional
+
                 -- Autocompletion
-                {'L3MON4D3/LuaSnip'},
-                {'hrsh7th/nvim-cmp'},
-                {'hrsh7th/cmp-nvim-lsp'},
-                {'hrsh7th/cmp-buffer'},
-                {'hrsh7th/cmp-path'},
-                {'hrsh7th/cmp-nvim-lua'},
-                {'saadparwaiz1/cmp_luasnip'},
-                {'onsails/lspkind-nvim'},
+                { "hrsh7th/nvim-cmp" }, -- Required
+                { "hrsh7th/cmp-nvim-lsp" }, -- Required
+                { "hrsh7th/cmp-buffer" }, -- Optional
+                { "hrsh7th/cmp-path" }, -- Optional
+                { "saadparwaiz1/cmp_luasnip" }, -- Optional
+                { "hrsh7th/cmp-nvim-lua" }, -- Optional
+
+                -- Snippets
+                { "L3MON4D3/LuaSnip" }, -- Required
+                { "rafamadriz/friendly-snippets" }, -- Optional
             },
             config = function()
                 require("config.lsp")
             end,
-        }
+        })
+
+        use({
+            "jose-elias-alvarez/null-ls.nvim",
+            after = "mason.nvim",
+            config = function()
+                require("config.null-ls")
+            end,
+        })
 
         -- Automatically set up your configuration after cloning packer.nvim
-        -- Put this at the end after all plugins
+        -- Must live end after all plugins
         if packer_bootstrap then
-            require('packer').sync()
+            require("packer").sync()
         end
     end,
 })
-
