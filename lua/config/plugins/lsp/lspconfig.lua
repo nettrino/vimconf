9506c40f3f1eea5f26237995f25117af6a5d1436
Theofilos Petsios <theofilos@crashoverride.com>
2024-03-14 12:29:29 +0200

    nit

--- lua/config/plugins/lsp/lspconfig.lua
+++ lua/config/plugins/lsp/lspconfig.lua
@@ -54,10 +54,10 @@ return {
 			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
 			opts.desc = "Show buffer diagnostics"
-			keymap.set("n", "<leader>gb", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+			keymap.set("n", "gb", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
 			opts.desc = "Show line diagnostics"
-			keymap.set("n", "<leader>gl", vim.diagnostic.open_float, opts) -- show diagnostics for line
+			keymap.set("n", "gl", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
 			opts.desc = "Go to previous diagnostic"
 			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
