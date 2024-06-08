92b8b5e20fccd6aa7c241f18a5a133b104744e91
Theofilos Petsios <theofilos@crashoverride.com>
2024-06-08 14:02:04 +0300

    disable treesitter on large files

--- lua/config/plugins/nvim-treesitter.lua
+++ lua/config/plugins/nvim-treesitter.lua
@@ -1,63 +1,70 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "windwp/nvim-ts-autotag",
-    },
-    config = function()
-      -- import nvim-treesitter plugin
-      local treesitter = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"windwp/nvim-ts-autotag",
+		},
+		config = function()
+			-- import nvim-treesitter plugin
+			local treesitter = require("nvim-treesitter.configs")
 
-      -- configure treesitter
-      treesitter.setup({ -- enable syntax highlighting
-        highlight = {
-          enable = true,
-        },
-        -- enable indentation
-        indent = { enable = true },
-        -- enable autotagging (w/ nvim-ts-autotag plugin)
-        autotag = {
-          enable = true,
-        },
-        -- ensure these language parsers are installed
-        ensure_installed = {
-                    "bash",
-                    "c",
-                    "css",
-                    "dockerfile",
-                    "gitignore",
-                    "go",
-                    "graphql",
-                    "html",
-                    "javascript",
-                    "json",
-                    "lua",
-                    "markdown",
-                    "markdown_inline",
-                    "prisma",
-                    "python",
-                    "query",
-                    "rust",
-                    "terraform",
-                    "tsx",
-                    "typescript",
-                    "vim",
-                    "vimdoc",
-                    "yaml",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-      })
-    end,
-  },
+			-- configure treesitter
+			treesitter.setup({ -- enable syntax highlighting
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+					-- use_languagetree = false,
+					disable = function(_, bufnr)
+						local buf_name = vim.api.nvim_buf_get_name(bufnr)
+						local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
+						return file_size > 2 * 1024
+					end,
+				},
+				-- enable indentation
+				indent = { enable = true },
+				-- enable autotagging (w/ nvim-ts-autotag plugin)
+				autotag = {
+					enable = true,
+				},
+				-- ensure these language parsers are installed
+				ensure_installed = {
+					"bash",
+					"c",
+					"css",
+					"dockerfile",
+					"gitignore",
+					"go",
+					"graphql",
+					"html",
+					"javascript",
+					"json",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"prisma",
+					"python",
+					"query",
+					"rust",
+					"terraform",
+					"tsx",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"yaml",
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+			})
+		end,
+	},
 }
