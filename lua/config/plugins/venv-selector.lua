fc0c15e893530cf58d3581d8618ca2bb18ddfefb
Theofilos Petsios <theofilos@crashoverride.com>
2024-06-12 12:06:48 +0300

    updated for 0.10

--- lua/config/plugins/venv-selector.lua
+++ lua/config/plugins/venv-selector.lua
@@ -1,16 +1,17 @@
 return {
 	"linux-cultist/venv-selector.nvim",
-	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-	opts = {
-		-- Your options go here
-		name = "venv",
-		auto_refresh = false,
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"mfussenegger/nvim-dap",
+		"mfussenegger/nvim-dap-python", --optional
+		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
 	},
-	event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+	lazy = false,
+	branch = "regexp", -- This is the regexp branch, use this for the new version
+	config = function()
+		require("venv-selector").setup()
+	end,
 	keys = {
-		-- Keymap to open VenvSelector to pick a venv.
-		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
-		-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-		{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+		{ ",v", "<cmd>VenvSelect<cr>" },
 	},
 }
