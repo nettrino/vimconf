8d69b4ae948a7c123e43af3a5f6757dbd148e2bd
Theofilos Petsios <theofilos@crashoverride.com>
2025-01-06 13:13:32 +0200

    remove after ftplugin

--- after/ftplugin/json.vim
+++
@@ -1,5 +0,0 @@
-set foldmethod=expr
-set foldexpr=nvim_treesitter#foldexpr()
-set foldlevel=99
-set foldlevelstart=1
-set foldnestmax=4
--- lua/config/plugins/formatting.lua
+++ lua/config/plugins/formatting.lua
@@ -27,8 +27,8 @@ return {
 				},
 			},
 			format_on_save = {
-				lsp_fallback = true,
-				timeout_ms = 1000,
+				lsp_fallback = false,
+				timeout_ms = 500,
 			},
 		})
 
