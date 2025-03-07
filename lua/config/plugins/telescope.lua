bd6e24bd3fa73a500921f80d95c1ae2b66037529
Theofilos Petsios <theofilos@crashoverride.com>
2025-03-07 06:07:25 -0500

    telescope

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,47 +1,39 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		local telescope = require("telescope")
-		local actions = require("telescope.actions")
-		local file_ignore_patterns = {
-			"yarn%.lock",
-			"node_modules/",
-			"dist/",
-			"%.next",
-			"build/",
-			"target/",
-			"package%-lock%.json",
-			"%.o",
-			"%.d",
-		}
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
+        local file_ignore_patterns = {
+            "yarn%.lock",
+            "node_modules/",
+            "dist/",
+            "%.next",
+            "build/",
+            "target/",
+            "package%-lock%.json",
+            "%.o",
+            "%.d",
+        }
 
-		telescope.setup({
-			defaults = {
-				path_display = { "truncate " },
-				file_ignore_patterns = file_ignore_patterns,
-				mappings = {
-					i = {
-						["<C-k>"] = actions.move_selection_previous, -- move to prev result
-						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-					},
-				},
-			},
-		})
+        telescope.setup({
+            defaults = {
+                path_display = { "truncate " },
+                file_ignore_patterns = file_ignore_patterns,
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                        ["<C-j>"] = actions.move_selection_next, -- move to next result
+                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                    },
+                },
+            },
+        })
 
-		telescope.load_extension("fzf")
-
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
-
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-	end,
+        telescope.load_extension("fzf")
+    end,
 }
