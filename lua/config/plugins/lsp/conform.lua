399cedcce272f6b74547105e63b72bdbe680003c
Theofilos Petsios <theofilos@crashoverride.com>
2025-07-14 16:34:04 +0300

    autoformat on save

--- lua/config/plugins/lsp/conform.lua
+++ lua/config/plugins/lsp/conform.lua
@@ -3,13 +3,17 @@ return {
     opts = {},
     formatters_by_ft = {
         lua = { "stylua" },
-        golang = { "golines" },
         -- Conform will run multiple formatters sequentially
         python = { "isort", "black" },
         -- You can customize some of the format options for the filetype (:help conform.format)
         rust = { "rustfmt", lsp_format = "fallback" },
         -- Conform will run the first available formatter
         javascript = { "prettierd", "prettier", stop_after_first = true },
+        go = { "gofumpt", "golines" },
+    },
+    formatters = {
+        ["goimports-reviser"] = { prepend_args = { "-rm-unused" } },
+        golines = { prepend_args = { "--max-len=80" } },
     },
     config = function()
         require("conform").setup({
